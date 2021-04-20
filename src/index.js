7ab5cd10e587c4f08aeb6bfbd72096694ecb4151
pepzarate <pepzarate@gmail.com>
2021-04-20 11:30:43 -0500

    Comenzando proyecto de react platzi video

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import HelloWorld from './components/HelloWorld'
+
+const mount = document.getElementById('app')
+
+ReactDOM.render(<HelloWorld />, mount)
